6b260036208a2047425c8c13f0e375d1d370ccf8
preanto <preanto15-5059@diu.edu.bd>
2024-10-19 00:40:23 +0600

    Test

---
+++ 1001.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a , b;
+    cin>>a>>b;
+    cout<<"x = "<< (a+b);
+    cout<<"\n";
+    return 0;
+}
+
+
---
+++ 1002.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main() {
+    double a;
+    cin>>a;
+    double b =(3.14159*a*a);
+    cout<<fixed<<setprecision(4)<<"A="<<b<<endl;
+    return 0;
+}
+
+
+
